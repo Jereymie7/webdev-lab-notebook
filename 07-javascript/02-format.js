e068ff25cb534656fbae342f878fb32a8621ea0c
jereymie <jereymie@pdx.edu>
2025-11-06 12:13:10 -0800

    07 Completed Exercises 1-4

--- 07-javascript/02-format.js
+++ 07-javascript/02-format.js
@@ -1,10 +1,13 @@
 const formatItems = (input) => {
   // return a string of the concatenated numbers, separated by commas and with three digits after the decimal point
-
+  let results = [];
+  for (let i = 0; i < input.length; i++) {
+    results.push(input[i].toFixed(3));
+  }
   // for reference, see the MDN Number page:
   // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number
 
-  return input;
+  return results.join(", ");
 };
 
 console.log(formatItems([1, 4, 17, 26, 41]));
